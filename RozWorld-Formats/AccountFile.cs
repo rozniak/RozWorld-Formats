8adbada441d5f6c6f754377a3f3940b6ed8bdc72
rozniak <roryfewell@hotmail.com>
2016-06-30 22:24:26 +0100

    Add AccountFile and update assembly info
    Very preliminary atm

---
+++ RozWorld-Formats/AccountFile.cs
@@ -0,0 +1,78 @@
+﻿/**
+ * Oddmatics.RozWorld.Formats.AccountFile -- RozWorld Account File
+ *
+ * This source-code is part of the file format I/O library for the RozWorld project by rozza of Oddmatics:
+ * <<http://www.oddmatics.uk>>
+ * <<http://roz.world>>
+ * <<http://github.com/rozniak/RozWorld-Formats>>
+ *
+ * Sharing, editing and general licence term information can be found inside of the "LICENCE.MD" file that should be located in the root of this project's directory structure.
+ */
+
+using Oddmatics.Util.IO;
+using System.IO;
+using System.Net;
+
+namespace Oddmatics.RozWorld.Formats
+{
+    /// <summary>
+    /// Represents a RozWorld user account file.
+    /// </summary>
+    public class AccountFile
+    {
+        /// <summary>
+        /// Gets the IPAddress that created this account.
+        /// </summary>
+        public IPAddress CreationIP { get; private set; }
+
+        /// <summary>
+        /// Gets the display name of this account.
+        /// </summary>
+        public string DisplayName { get; private set; }
+
+        /// <summary>
+        /// Gets the last logged in IPAddress of this account.
+        /// </summary>
+        public IPAddress LastLogInIP { get; private set; }
+
+        /// <summary>
+        /// Gets the password hash of this account.
+        /// </summary>
+        public byte[] PasswordHash { get; private set; }
+
+        /// <summary>
+        /// Gets the source filename of this account.
+        /// </summary>
+        public string Source { get; private set; }
+
+        /// <summary>
+        /// Gets the username of this account.
+        /// </summary>
+        public string Username { get; private set; }
+
+
+        /// <summary>
+        /// Initialises a new instance of the AccountFile class with a filename to load from.
+        /// </summary>
+        /// <param name="filename">The filename of the account to load.</param>
+        public AccountFile(string filename)
+        {
+            // Load here
+        }
+
+        /// <summary>
+        /// Initialises a new instance of the AccountFile class with a username, password hash and creation IPAddress.
+        /// </summary>
+        /// <param name="username">The username to set for this account.</param>
+        /// <param name="passwordHash">The password to set for this account.</param>
+        /// <param name="creationIP">The creation IP of this account.</param>
+        public AccountFile(string username, byte[] passwordHash, IPAddress creationIP)
+        {
+            CreationIP = creationIP;
+            DisplayName = username;
+            LastLogInIP = creationIP;
+            PasswordHash = passwordHash;
+            Username = username;
+        }
+    }
+}
---
+++ RozWorld-Formats/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RozWorld Formats")]
+[assembly: AssemblyDescription("Library for reading/writing RozWorld related file formats.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oddmatics")]
+[assembly: AssemblyProduct("RozWorld Formats")]
+[assembly: AssemblyCopyright("Copyright Oddmatics ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04d93a87-8231-4533-94d4-05e648a530de")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]

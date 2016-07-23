4d9fa527070687a1909bcc6f48378f9ce14dbac7
rozniak <roryfewell@hotmail.com>
2016-07-23 13:40:43 +0100

    Add PermissionGroupFile, add Json.NET libs
    Permission files use JSON to store their data, just fyi. :+1:

---
+++ RozWorld-Formats/PermissionGroupFile.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Oddmatics.Util.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Oddmatics.RozWorld.Formats
+{
+    /// <summary>
+    /// Represents a permission group JSON file.
+    /// </summary>
+    public class PermissionGroupFile
+    {
+        /// <summary>
+        /// Gets the colour of the names in game chat for members of this group.
+        /// </summary>
+        public string Colour { get; set; }
+
+        /// <summary>
+        /// Gets whether this permission group is to be the default.
+        /// </summary>
+        public bool Default { get; set; }
+
+        /// <summary>
+        /// Gets the name of this permission group.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the permissions granted by this group.
+        /// </summary>
+        public string[] Permissions { get; set; }
+
+        /// <summary>
+        /// Gets the game chat prefix to apply to members of this group.
+        /// </summary>
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// Gets the game chat suffix to apply to members of this group.
+        /// </summary>
+        public string Suffix { get; set; }
+
+
+        /// <summary>
+        /// Creates a PermissionGroupFile from the specified file using JSON data in that file.
+        /// </summary>
+        /// <param name="filename">A string that contains the name of the file from which to create the PermissionGroupFile.</param>
+        /// <returns>The PermissionGroupFile this method creates.</returns>
+        public static PermissionGroupFile FromFile(string filename)
+        {
+            var data = FileSystem.GetTextFile(filename);
+            string dataAsString = String.Empty;
+
+            foreach (string line in data)
+            {
+                dataAsString += line + "\n";
+            }
+
+            return JsonConvert.DeserializeObject<PermissionGroupFile>(dataAsString);
+        }
+
+
+        /// <summary>
+        /// Saves this PermissionGroupFile as JSON to the specified location.
+        /// </summary>
+        /// <param name="filename">The location to save to.</param>
+        public void Save(string filename)
+        {
+            string data = JsonConvert.SerializeObject(this);
+            FileSystem.PutTextFile(filename, new string[] { data });
+        }
+    }
+}
